a6fc82230bbc99e2fd981c043303ebd284adb7c0
Sery0077 <52316851+Sery0077@users.noreply.github.com>
2023-03-07 23:17:57 +0300

    add splash screen and resources

--- app/src/main/java/sery/vlasenko/betmatch/MainActivity.kt
+++ app/src/main/java/sery/vlasenko/betmatch/MainActivity.kt
@@ -1,11 +1,21 @@
 package sery.vlasenko.betmatch
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.background_layout)
+
+        delayAndSetContentView()
+    }
+
+    private fun delayAndSetContentView() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            setContentView(R.layout.activity_main)
+        }, 3000)
     }
 }
