9fb78f275c589b364b702994e9b8ec7399c4bf76
Sery0077 <52316851+Sery0077@users.noreply.github.com>
2023-03-09 01:38:13 +0300

    refactor, add changing theme, wallpapers

--- app/src/main/java/sery/vlasenko/betmatch/MainActivity.kt
+++ app/src/main/java/sery/vlasenko/betmatch/MainActivity.kt
@@ -1,5 +1,6 @@
 package sery.vlasenko.betmatch
 
+import android.content.Context
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -10,6 +11,8 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setBackground()
+
         setContentView(R.layout.background_layout)
 
         delayAndSetContentView()
@@ -18,6 +21,13 @@ class MainActivity : AppCompatActivity() {
     private fun delayAndSetContentView() {
         Handler(Looper.getMainLooper()).postDelayed({
             setContentView(R.layout.activity_main)
-        }, 0)
+        }, 3000)
+    }
+
+    private fun setBackground() {
+        val pf = getSharedPreferences(getString(R.string.prefs), Context.MODE_PRIVATE)
+        val backgroundId = pf.getInt(getString(R.string.pref_background_id), R.drawable.background_1)
+
+        window?.setBackgroundDrawableResource(backgroundId)
     }
 }
